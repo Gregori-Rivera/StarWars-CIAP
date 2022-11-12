8b7782428202384b51e741ba92ed97752252c062
Gregori-Rivera <gregori.rivera@gmail.com>
2022-11-12 05:15:41 +0000

    codigo de characters, planets, vehicles

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -4,34 +4,40 @@ import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
-import { Single } from "./pages/single";
+import { PageCharacters } from "./pages/pageCharacters";
+import { PagePlanets } from "./pages/pagePlanets";
+import { PageVehicles } from "./pages/pageVehicles";
+
 import injectContext from "./store/appContext";
 
-import { Navbar } from "./component/navbar";
+// import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
 //create your first component
 const Layout = () => {
-    //the basename is used when your project is published in a subdirectory and not in the root of the domain
-    // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-    const basename = process.env.BASENAME || "";
+  //the basename is used when your project is published in a subdirectory and not in the root of the domain
+  // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+  const basename = process.env.BASENAME || "";
 
-    return (
-        <div>
-            <BrowserRouter basename={basename}>
-                <ScrollToTop>
-                    <Navbar />
-                    <Routes>
-                        <Route element={<Home />} path="/" />
-                        <Route element={<Demo />} path="/demo" />
-                        <Route element={<Single />} path="/single/:theid" />
-                        <Route element={<h1>Not found!</h1>} />
-                    </Routes>
-                    <Footer />
-                </ScrollToTop>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div>
+      <BrowserRouter basename={basename}>
+        <ScrollToTop>
+          {/* <Navbar /> */}
+          <Routes>
+            {/* enrutamiento asignamos la url con extencion a cada componente del sitio */}
+            <Route element={<Home />} path="/" />
+            <Route element={<Demo />} path="/demo" />
+            <Route element={<PageCharacters />} path="/pageCharacters/:id" />
+            <Route element={<PagePlanets />} path="/pagePlanets/:id" />
+            <Route element={<PageVehicles />} path="/pageVehicles/:id" />
+            <Route element={<h1>Not found!</h1>} />
+          </Routes>
+          <Footer />
+        </ScrollToTop>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default injectContext(Layout);
